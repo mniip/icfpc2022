dfcf05cea36e4f382ea72a3006deb7736e9a19f2
mniip <mniip@mniip.com>
2022-09-02 21:14:15 +0300

    Add renderer executable

---
+++ app/Render.hs
@@ -0,0 +1,24 @@
+import Codec.Picture
+import Data.List.NonEmpty qualified as NE
+import Data.Monoid
+import Data.Text.IO qualified as T
+import System.Environment
+import Text.Read
+
+import ICFPC.ISL
+import ICFPC.Render
+import ICFPC.Tracer
+
+main :: IO ()
+main = do
+  getArgs >>= \case
+    [sWidth, sHeight, output]
+      | Just width <- readMaybe sWidth
+      , Just height <- readMaybe sHeight
+      -> parseProgram <$> T.getContents >>= \case
+        Left err -> error err
+        Right prog -> case runRender (XY width height) (runTrace prog (XY width height)) of
+          ((Nothing, _), image) -> savePngImage output $ ImageRGBA8 image
+          ((Just err, BState line _ _), _) -> error $ show err <> "\nOn line " <> show (line + 1)
+            <> "\n" <> case prog of Program ls -> show $ ls NE.!! line
+    _ -> error "Usage: render <width> <height> <output.png>"
---
+++ lib/ICFPC/Render.hs
@@ -0,0 +1,42 @@
+module ICFPC.Render where
+
+import Codec.Picture.Types
+import Control.Monad.ST
+import Control.Monad.Reader
+import Data.Vector.Storable.Mutable qualified as V
+
+import ICFPC.Tracer
+
+newtype RenderM s a = RenderM { runRenderM :: MutableImage s PixelRGBA8 -> ST s a }
+  deriving (Functor, Applicative, Monad)
+    via (ReaderT (MutableImage s PixelRGBA8) (ST s))
+
+instance MonadCommand (RenderM s) where
+  onXCut _ _ = pure ()
+  onYCut _ _ = pure ()
+  onPCut _ = pure ()
+  onColor (XY (MinMax xmin xmax) (MinMax ymin ymax)) (r, g, b, a) = do
+    forM_ [ymin .. ymax - 1] $ \y ->
+      forM_ [xmin .. xmax - 1] $ \x ->
+        RenderM $ \image -> writePixel image x y rgba
+    where
+      rgba = PixelRGBA8 (fromIntegral r) (fromIntegral g) (fromIntegral b) (fromIntegral a)
+  onSwap (XY (MinMax xmin1 xmax1) (MinMax ymin1 ymax1)) (XY (MinMax xmin2 _) (MinMax ymin2 _)) = do
+    forM_ [0 .. ymax1 - ymin1 - 1] $ \dy ->
+      forM [0 .. xmax1 - xmin1 - 1] $ \dx ->
+        RenderM $ \image -> do
+          c1 <- readPixel image (xmin1 + dx) (ymin1 + dy)
+          c2 <- readPixel image (xmin2 + dx) (ymin2 + dy)
+          writePixel image (xmin2 + dx) (ymin2 + dy) c1
+          writePixel image (xmin1 + dx) (ymin1 + dy) c2
+  onXMerge _ _ = pure ()
+  onYMerge _ _ = pure ()
+
+runRender :: XY Int -> (forall s. RenderM s a) -> (a, Image PixelRGBA8)
+runRender (XY width height) f = runST $ do
+  image <- newMutableImage width height
+  forM_ [0 .. width - 1] $ \y ->
+    forM [0 .. height - 1] $ \x ->
+      writePixel image x y (PixelRGBA8 255 255 255 255)
+  r <- runRenderM f image
+  (r,) <$> unsafeFreezeImage image
